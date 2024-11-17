c8f550f6416500bbd1f43f2543e854d2c10cea39
Alex Southgate <southgateJA@cardiff.ac.uk>
2024-11-17 17:32:27 +0000

    add optimisation module and a simple example test

---
+++ R/optimisation.R
@@ -0,0 +1,34 @@
+#' Exponential function with an affine exponent in x
+#'
+#' Calculates the time before the next event as:
+#'  x^(a + x * b)
+#'
+#' @param x Input sequence
+#' @param a Constant exponent
+#' @param b Scalar for exponent
+#'
+#' @return Time in seconds until next event is scheduled.
+exponential_affine_exponent <- function(x, a, b) {
+    x^(a + b * x)
+}
+
+
+# TODO: support online optimization
+
+#' TimeDeltaCalculator
+#'
+#' Calculates future event times
+#'
+#' @import R6A
+#' @export
+TimeDeltaCalculator <- R6::R6Class(
+    "TimeDeltaCalculator",
+    public = list(
+        initialize = function() {
+
+        },
+        cal_delta = function(x) {
+            exponential_affine_exponent(x, 1.0, 0.5) 
+        }
+    )
+)
---
+++ tests/testthat/test_optimisation.R
@@ -0,0 +1,8 @@
+library(testthat)
+
+test_that("TimeDeltaCalculator produces expected results", {
+    tdc <- TimeDeltaCalculator$new()
+    x <- 1:3
+    exp <- c(1.00000, 4.00000, 15.58846)
+    expect_equal(tdc$cal_delta(x), exp, tolerance = 1e-3)
+})

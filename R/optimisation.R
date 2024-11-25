95a8505919d44edc906a043aa1a9c8ea6abd8ab2
Alex Southgate <southgateJA@cardiff.ac.uk>
2024-11-25 11:17:32 +0000

    implement polynomial memory func and a more robust tests independent of some sensible parameterisation

--- R/optimisation.R
+++ R/optimisation.R
@@ -9,15 +9,23 @@ TimeDeltaCalculator <- R6::R6Class(
     "TimeDeltaCalculator",
     public = list(
         initialize = function() {
-
         },
         cal_time_delta = function(x) {
-            private$expf(x, 1.0, 0.5) 
+#            private$expf(x, 1.0, 0.5) 
+            private$polyf(x)
         }
     ),
     private = list(
         expf = function(x, a, b) {
             x^(a + b * x)
-        }
+        },
+        polyf = function(x) {
+            pows <- 1:length(private$w) 
+            outer <- outer(x, pows, function(x, a) x^a)
+            result <- outer %*% private$w
+            result
+        },
+        w = c(1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01)
     )
 )
+
--- tests/testthat/test_optimisation.R
+++ tests/testthat/test_optimisation.R
@@ -2,7 +2,13 @@ library(testthat)
 
 test_that("TimeDeltaCalculator produces expected results", {
     tdc <- TimeDeltaCalculator$new()
-    x <- 1:3
-    exp <- c(1.00000, 4.00000, 15.58846)
-    expect_equal(tdc$cal_time_delta(x), exp, tolerance = 1e-3)
+    x <- 1:13
+    res <- tdc$cal_time_delta(x)
+    for (i in 2:length(res)) {
+        expect_gt(res[i], res[i-1])
+    }
+    expect_gt(res[length(res)], 5000)
+    res <- tdc$cal_time_delta(x)
+    expect_gt(tdc$cal_time_delta(2), tdc$cal_time_delta(1))
+    expect_gt(tdc$cal_time_delta(1), tdc$cal_time_delta(0))
 })
